837c7a479da790173a507deb44a11dfd320914ae
savioshaju <savioshajum@gmail.com>
2025-11-04 16:49:33 +0530

    deploy

--- webapp/app.py
+++ webapp/app.py
@@ -111,8 +111,7 @@ def static_files(filename):
     return send_from_directory("static", filename)
 
 if __name__ == "__main__":
-    # Use env PORT if provided by platform; default 5000
     port = int(os.environ.get("PORT", 5000))
-    # Force non-debug in production
     app.run(host="0.0.0.0", port=port, debug=False)
+
  
